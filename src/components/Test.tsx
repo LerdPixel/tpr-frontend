db9c450ce8b7a02f6332e68f311ebabadc9a181a
Ilya Grechko <laminariakua@gmail.com>
2025-06-04 16:55:36 +0300

    questions naming

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -5,28 +5,20 @@ import NumericAnswer from './questions/NumericAnswer.tsx'
 import SortableQuestion from './questions/SortableQuestion.tsx'
 
 const questionTypes = {
-  single: "Один ответ",
-  multiple: "Несколько вариантов",
-  short: "Краткий ответ",
-  numeric: "Числовой",
-  sortable: "Ранжирующий",
-  matching: "Соответствие",
+  single: ["Один ответ", "Создание вопроса с одним вариантом ответа"],
+  multiple: ["Несколько вариантов", "Создание вопроса с несколькими ответами"],
+  short: ["Краткий ответ", "Создание вопроса с коротким ответом"],
+  numeric: ["Числовой", "Создание вопроса с численным ответом"],
+  sortable: ["Ранжирующий","Создание вопроса на упорядочивание"],
+  matching: ["Соответствие","Создание вопроса на соответствие"],
 } as const;
 
 type QuestionType = keyof typeof questionTypes;
 
 const QuestionCreator = () => {
   const [questionType, setQuestionType] = useState<QuestionType>('single');
-  const [cost, setCost] = useState(22);
-  function RusWordEnding(n : number) {
-    if ([11, 12, 13, 14].includes(n))
-      return "ов";
-    else if (n % 10 === 1)
-      return "";
-    else if ([2, 3, 4].includes(n % 10))
-      return "а"
-    return "ов"
-  }
+  const [cost, setCost] = useState(1);
+
   const renderQuestionComponent = () => {
     console.log(questionType);
     
@@ -49,9 +41,7 @@ const QuestionCreator = () => {
     <div className={styles.container}>
       <div className={styles.header}>
         <h2 className={styles.title}>
-          {questionType === 'single'
-            ? 'Создание вопроса с одним вариантом ответа'
-            : 'Вопрос с численным ответом'}
+          {questionTypes[questionType][1]}
         </h2>
         <div className={styles.points}>
           <span className={styles.pointsValue}>{cost}</span> балл{RusWordEnding(cost)}
@@ -77,7 +67,7 @@ const QuestionCreator = () => {
           onChange={(e) => setQuestionType(e.target.value as QuestionType)}
         >
           {Object.keys(questionTypes).map((key) =>
-            <option key={key} value={key}>{questionTypes[key]}</option>
+            <option key={key} value={key}>{questionTypes[key][0]}</option>
           )}
           
           <option value="numeric">Численный</option>
@@ -87,7 +77,15 @@ const QuestionCreator = () => {
   );
 };
 
-
+function RusWordEnding(n : number) {
+  if ([11, 12, 13, 14].includes(n))
+    return "ов";
+  else if (n % 10 === 1)
+    return "";
+  else if ([2, 3, 4].includes(n % 10))
+    return "а"
+  return "ов"
+}
 
 
 
